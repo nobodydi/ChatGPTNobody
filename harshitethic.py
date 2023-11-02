cf9157571890b40d7053f2f80907d0418eb5c4f7
nobodydi <149184961+nobodydi@users.noreply.github.com>
2023-11-02 15:15:21 +0530

    Update config.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "6899668612:AAEYfCLysj4Y612TY-MTGMdGpgSR6xIRwOk"
 else:
     tgkey = tgenv
 print(tgenv)
@@ -33,8 +33,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'Harshit ethic'
-username = 'harshitethic_bot'
+botname = 'EMILYCHATGPTBOT'
+username = 'EMILYCHATGPTBOT'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
